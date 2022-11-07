3524a1cb1fdf8b3f7ab0a081a2175d9d556c1500
KavithaVMG <117630137+KavithaVMG@users.noreply.github.com>
2022-11-07 13:58:56 +0530

    Add files via upload

---
+++ Server/Service/EmployeeService.cs
@@ -0,0 +1,14 @@
+﻿using Common;
+using DAL;
+
+namespace Service
+{
+    public class EmployeeService
+    {
+        public EmployeeDTO SaveEmployee(EmployeeDTO employee)
+        {
+            return new EmployeeDAL().SaveEmployee(employee);
+        }
+
+    }
+}
