c2c19866b18504b56aa8842f37ad0d3caed87c90
KavithaVMG <117630137+KavithaVMG@users.noreply.github.com>
2022-11-07 13:56:00 +0530

    Add files via upload

---
+++ Server/DAL/EmployeeDAL.cs
@@ -0,0 +1,40 @@
+﻿using Common;
+using Newtonsoft.Json;
+
+namespace DAL
+{
+    public class EmployeeDAL
+    {
+        public EmployeeDTO SaveEmployee(EmployeeDTO employee)
+        {
+            try
+            {
+                string docPath = Environment.CurrentDirectory;
+                string path = Path.Combine(docPath, Constants.EmployeeFileName);
+                List<EmployeeDTO> list = new List<EmployeeDTO>();
+                if (File.Exists(path))
+                {
+                    var employeeRead = File.ReadAllText(path);
+
+                    if (employeeRead != null)
+                    {
+                        list = JsonConvert.DeserializeObject<List<EmployeeDTO>>(employeeRead);
+
+                    }
+                }
+
+                if (employee != null)
+                    list.Add(employee);
+
+                File.WriteAllText(path, JsonConvert.SerializeObject(list));
+                return employee;
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+
+        }
+    }
+}
---
+++ Server/EmployeeAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,40 @@
+﻿using Common;
+using Microsoft.AspNetCore.Mvc;
+using Service;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace EmployeeAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        // GET: api/<EmployeeController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<EmployeeController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<EmployeeController>
+        [HttpPost]
+        public IActionResult Post([FromBody] EmployeeDTO employee)
+        {
+            return Ok(new EmployeeService().SaveEmployee(employee));
+        }
+
+        // DELETE api/<EmployeeController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
